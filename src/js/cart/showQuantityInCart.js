e547398433541420a7797ad1e29b974f6c58a54f
Tanjushkin-Bondarenko <91343388+Tanjushkin-Bondarenko@users.noreply.github.com>
2025-11-10 01:19:29 +0200

    Update showQuantityInCart.js

--- src/js/cart/showQuantityInCart.js
+++ src/js/cart/showQuantityInCart.js
@@ -4,7 +4,7 @@ export function showQuantityInCart(){
   let quantityProductsInCart = document.querySelector("#quantity-in-card");
   let saveProducts = JSON.parse(localStorage.getItem("product")) || [];
    let sum = saveProducts.reduce((item, acc)=>{
-    return acc += Number(item.quantity)
+    return acc + Number(item.quantity)
   }, 0)
   if(quantityProductsInCart.style.display == "none" && sum>0){
     quantityProductsInCart.style.display = "inline-block"
