d157ac6cf30f60c322605caa3845686345cff5f0
Tanjushkin-Bondarenko <91343388+Tanjushkin-Bondarenko@users.noreply.github.com>
2025-11-09 23:30:16 +0200

    Update showQuantityInCart.js

--- src/js/cart/showQuantityInCart.js
+++ src/js/cart/showQuantityInCart.js
@@ -3,10 +3,9 @@ import { openCart } from "./openCart.js";
 export function showQuantityInCart(){
   let quantityProductsInCart = document.querySelector("#quantity-in-card");
   let saveProducts = JSON.parse(localStorage.getItem("product")) || [];
-  let sum = 0
-  saveProducts.map(item=>{
-    return sum += Number(item.quantity)
-  })
+   let sum = saveProducts.reduce((item, acc)=>{
+    return acc += Number(item.quantity)
+  }, 0)
   if(quantityProductsInCart.style.display == "none" && sum>0){
     quantityProductsInCart.style.display = "inline-block"
     quantityProductsInCart.textContent = sum;
